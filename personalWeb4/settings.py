5bb24e73a8180b6ec66c0cd10e8fe13f96228f80
alvin saini <codewithalvin@gmail.com>
2021-07-09 22:52:41 +0530

    end touch

--- personalWeb4/settings.py
+++ personalWeb4/settings.py
@@ -133,14 +133,6 @@ STATICFILES_DIRS=[
 		os.path.join(BASE_DIR,"static/"),
 			]
 
-# email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = os.environ.get("mail")
-EMAIL_HOST_PASSWORD = os.environ.get("passkey")
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
 # environment setup
 if os.environ.get("PRODUCTION")=="TRUE":

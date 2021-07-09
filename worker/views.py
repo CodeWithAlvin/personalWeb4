bb004a22f2130b4c3d0adf055f2c5b40834b5892
alvin saini <codewithalvin@gmail.com>
2021-07-09 22:01:45 +0530

    all logic compleated remaining to upload projects

--- worker/views.py
+++ worker/views.py
@@ -5,4 +5,5 @@ from worker.models import Project,Blog
 def index(request):
 	context={"Blogs":Blog.objects.values(),
 				"Projects":Project.objects.values()}
+				
 	return render(request,"index.html",context=context)
